2ad4c57815929fd86c0f8686729b2d8835e05151
tiwkao <pongsakorn.592040105@gmail.com>
2026-03-13 16:14:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // นำ URL ที่ได้จากการ Deploy "Web App" ใน Google Apps Script มาใส่ที่นี่
-const API_URL = "https://script.google.com/macros/s/AKfycbw6gb8BfAkTfpUHDv-AFiZSEaxC5FRnNcFKGLNlJ1BKovOjNQHkTzM0qQ9Dkc7EZxEk/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyZpbOUwvo5BxlsdExiDeurg5QhGmk5Rh_PJCxenSrEnZhM4oGyP832uI8DOxLJrRMT/exec";
